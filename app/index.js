0f57c44b01991421da3d47ced7b7b888259aa9f7
JE <casaval@gmail.com>
2016-07-18 23:22:53 +0000

    initial version. Still todo: graphics, reset game

---
+++ app/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const _ = require('lodash');
+const Vue = require('vue');
+
+let bag = _.range(1,37);
+bag = _.shuffle(bag);
+console.log('bag:', bag);
+
+let played = [];
+
+module.exports = function KarubaTilesApplication(){
+    new Vue({
+      el: '#app',
+      data: {
+          bag: bag,
+          played: played
+      },
+      methods: {
+        drawTile: function () {
+            console.log('drawing tile...');
+            let tile
+            if(bag.length){
+                tile = _.pullAt(bag,0);
+            }else{
+                tile = 'NO MORE TILES';
+            }
+            played.push(tile);
+        },
+        reset: function () {
+            console.log('resetting game state...');
+        }
+      }
+    })
+};
---
+++ make.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('Starting up...');
+
+
+
+
+console.log('Loading application...');
+const App = require('./app')();
